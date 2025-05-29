926c05126e9907977ae3708ee5c88abe475962a0
hieu30052001 <koconik111@gmail.com>
2025-05-29 10:20:23 +0700

    Update back.js

--- api/back.js
+++ api/back.js
@@ -14,7 +14,7 @@ mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const logSchema = new mongoose.Schema({
   username: { type: String, required: true },
   ca: { type: String, required: true },
- may: { type: String, required: true },
+  may: { type: String, required: true },
   startTime: { type: Date, required: true },
   endTime: { type: Date, required: true },
   error: { type: String, required: true },
@@ -31,7 +31,7 @@ app.use(express.json());
 app.post('/api/back', async (req, res) => {
   const { username, ca, may, startTime, endTime, error, errorDuration } = req.body;
 
-  if (!username || !ca || !startTime || !endTime || !error || !errorDuration) {
+  if (!username || !ca || !may || !startTime || !endTime || !error || !errorDuration) {
     return res.status(400).json({ error: 'Dữ liệu không hợp lệ.' });
   }
 
